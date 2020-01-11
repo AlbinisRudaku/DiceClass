fcd5c2794796a27f2039497e7669086dd2ce60e9
Albinis Rudaku <ruddaku@gmail.com>
2020-01-11 12:19:33 +0100

    Update DiceController.java

--- DiceController.java
+++ DiceController.java
@@ -1,23 +1,20 @@
-/** Klasa DieClass e bën kontrollimin e hedhjes së zarit me anë të metodave */
+/** This class controls the process of throwing the dices through its methods */
 public class DiceController
 {
-    /**
-     * private DiceModel model bën deklarimin e klasës DiceModel
-     * private DiceView view bën deklarimin e klasës DiceView
-     */
+    /** private DiceModel model declares the DiceModel class
+     *  private DiceView view declares the DiceView class */
     private DiceModel model;
     private DiceView view;
 
-    /** Metoda kontroller DiceController e bën deklarimin e klasës DiceController
-     * me DiceModel dhe DiceView */
+    /** The DiceController controller initializes variables such as view and model */
     public  DiceController(DiceModel model, DiceView view)
     {
         this.model = model;
         this.view = view;
     }
 
-    /** Nëse askForInput  është më i madh se 0,atëherë metoda throwDice() bën hedhjen e zareve
-     *  në mënyrë të rëndomt duke përdorur Math.random() dhe bën afishimin i rezultatit të fituar */
+    /** If the method askForInput is 0, then method throwDice() generates the number as shown before,
+     *  and displays it as the final result */
     public void throwDice()
     {
         int dice1 = model.throwDice();
